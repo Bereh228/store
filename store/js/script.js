aeb7426d991457fbd5c77a92c09b77b1e5e4499a
Bereh228 <andrey.berezhko999@gmail.com>
2021-06-21 20:04:32 +0300

    s/e

--- store/js/script.js
+++ store/js/script.js
@@ -1,24 +1,55 @@
+const header = document.getElementById('header');
+const workTime = document.getElementById('workTime');
+const openMes = document.querySelector('.openMes');
+const closeMes = document.querySelector('.closeMes');
 
-// const header = document.getElementById('header');
+function checkScrollAndHeader(){
+    let scrollY = window.pageYOffset;
 
-// function checkScrollAndHeader(){
-//     let scrollY = window.pageYOffset;
+    if(scrollY > 500){
+        header.classList.add('header_fixed');
+    }
+    else{
+        header.classList.remove('header_fixed');
+    }
+}
 
-//     if(scrollY > 500){
-//         header.classList.add('header_fixed');
-//     }
-//     else{
-//         header.classList.remove('header_fixed');
-//     }
-// }
+function showOpen(){
+    openMes.classList.add('openMes_active');
+    // delete 
+    setTimeout(() => {
+        openMes.classList.remove('openMes_active');
+    }, 5000);
+}
+
+function showExit(){
+    closeMes.classList.add('closeMes_active');
+    // delete 
+    setTimeout(() => {
+        closeMes.classList.remove('closeMes_active');
+    }, 5000);
+}
+
+document.addEventListener('DOMContentLoaded',checkScrollAndHeader)
+window.addEventListener('scroll',checkScrollAndHeader);
 
 
+workTime.addEventListener('mouseover', function(){
+   let date = new Date();
 
-// document.addEventListener('DOMContentLoaded',checkScrollAndHeader)
+   if(date.getHours() >= 9 && date.getHours()< 18){
+    showOpen();
+   }
+   else if(date.getHours() == 18){
+       if(date.getMinutes() == 0){
+        showOpen();
+       }
+       else{
+        showExit();
+       }
+   }
+   else{
+    showExit();
+   }
 
-// window.addEventListener('scroll',checkScrollAndHeader);
-
-
-
-
-      
+})
