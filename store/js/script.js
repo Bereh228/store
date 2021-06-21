8066f09dd25c0cf0fd9b4e521224fb2e4b808161
Bereh228 <andrey.berezhko999@gmail.com>
2021-06-21 14:43:27 +0300

    add section collection

---
+++ store/js/script.js
@@ -0,0 +1,18 @@
+// const header = document.getElementById('header');
+
+// function checkScrollAndHeader(){
+//     let scrollY = window.pageYOffset;
+
+//     if(scrollY > 500){
+//         header.classList.add('header_fixed');
+//     }
+//     else{
+//         header.classList.remove('header_fixed');
+//     }
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded',checkScrollAndHeader)
+
+// window.addEventListener('scroll',checkScrollAndHeader);
